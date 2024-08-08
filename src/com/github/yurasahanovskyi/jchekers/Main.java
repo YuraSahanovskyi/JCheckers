a5075f3c6d0919e6150c5c88bc34ea2e1fb0b587
Yura Sahanovskyi <yuriisahanovskyi@gmail.com>
2024-08-08 17:14:51 +0300

    Add greeting message

--- src/com/github/yurasahanovskyi/jchekers/Main.java
+++ src/com/github/yurasahanovskyi/jchekers/Main.java
@@ -1,7 +1,8 @@
 package com.github.yurasahanovskyi.jchekers;
 
 public class Main {
+    private static final String GREETING = "*****Welcome to JCheckers!*****";
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(GREETING);
     }
 }
